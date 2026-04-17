790879fb62550f5bbc1ad6ff1a7153a280c171a8
Nha Uyen N. <nuyen959@gmail.com>
2026-04-17 15:02:36 +0700

    Fix words

--- src/test/java/com/tester/Bai5_Locators/DemoHTMLAttribute.java
+++ src/test/java/com/tester/Bai5_Locators/DemoHTMLAttribute.java
@@ -2,6 +2,6 @@ package com.tester.Bai5_Locators;
 
 public class DemoHTMLAttribute {
     public static void main(String[] args) {
-        System.out.println("Locator HTML Atteibute.");
+        System.out.println("Locator HTML Attribute.");
     }
 }
